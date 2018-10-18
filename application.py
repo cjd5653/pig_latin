c5d60a5ea26f1ffdbcc551fa9ff2fff7f2789b9c
cjd5653 <cjd5653@psu.edu>
2018-10-17 22:51:54 -0400

    Added translation to pig latin functionality, cleaned up print formatting

--- application.py
+++ application.py
@@ -1,6 +1,36 @@
 # Connor Deardorff
 # DevPSU Project 4
 
+
+# --Helper Function piggify()--
+# Translates a given word to pig latin
+def piggify(word):
+    # Get rid of newline character from file, if present
+    word = word.strip('\n')
+
+    vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+
+    # Find if word starts with a vowel
+    if word[0] in vowels:
+        # Starts with vowel, return word plus "yay"
+        return word + "yay";
+    else:
+        # Starts with consonant, so manipulate the word
+        first_vowel = 0
+        while word[first_vowel] not in vowels:
+            first_vowel += 1
+
+        new_word = word[first_vowel]
+        for index in range(first_vowel + 1, len(word)):
+            new_word += word[index]
+
+        for index in range(0, first_vowel):
+            new_word += word[index]
+
+        return new_word + "ay"
+# --End piggify()--
+
+
 # --Main function--
 # FILENAME: Path and name of file to be read
 filename = "sample_text"
@@ -10,7 +40,17 @@ file = open(filename)
 
 # Read each line of the file individually
 for text in file:
-    print(text)
+    # Split words apart
+    words = text.split(" ")
+
+    # Translate each word into pig latin
+    for i in range(0, len(words)):
+        words[i] = piggify(words[i])
+
+    # Print translated words
+    for word in words:
+        print(word, end=' ')
+    print('')
 
 # Close the file
 file.close()
